5b21018ae242099a45346abe7c5a2a18488e3ca2
teo-crth <teoconrath@gmail.com>
2024-01-08 15:38:04 +0100

    first commit

---
+++ src/header.php
@@ -0,0 +1,22 @@
+<header id="header">
+            <h1 class="centrer">Mon CV en Ligne</h1>
+            <h2 id="h2Header" class="centrer">Téo Conrath - Développeur web</h2>
+
+            <!--MENU-->
+            <nav id="navbar">
+                <ul id="menu">
+                    <li class="menuhoverFormations" id="ongletFormation">
+                        <a href="index.html">Formations</a>
+                    </li>
+                    <li class="menuhoverExperiences">
+                        <a href="experiences.html">Expériences</a>
+                    </li>
+                    <li class="menuhoverCompetences">
+                        <a href="competences.html">Compétences</a>
+                    </li>
+                    <li class="menuhoverContact">
+                        <a href="contact.html">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
