e4fce2836b2a594dc6d1476aaa79b84d6f5888b4
teo-crth <teoconrath@gmail.com>
2025-01-14 11:44:25 +0100

    projet au detour des mots images

--- js/sectionProjects.js
+++ js/sectionProjects.js
@@ -1,32 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
     
-    const cardOpAndCo =
-        {
-            siteImages: ['../img/captures-sites/op-1.png', '../img/captures-sites/op-2.png', '../img/captures-sites/op-3.png', '../img/captures-sites/op-4.png'],
-            siteImagesMobile: ['../img/captures-sites/op-1-mobile.png', '../img/captures-sites/op-service-mobile.png', '../img/captures-sites/op-faq-mobile.png', '../img/captures-sites/op-suisje-mobile.png', '../img/captures-sites/op-tarifaccueil-mobile.png'],
-        };
+    const cardOpAndCo = {
+        siteImages: ['../img/captures-sites/op-1.png', '../img/captures-sites/op-2.png', '../img/captures-sites/op-3.png', '../img/captures-sites/op-4.png'],
+        siteImagesMobile: ['../img/captures-sites/op-1-mobile.png', '../img/captures-sites/op-service-mobile.png', '../img/captures-sites/op-faq-mobile.png', '../img/captures-sites/op-suisje-mobile.png', '../img/captures-sites/op-tarifaccueil-mobile.png'],
+    };
+
+    const cardAuDetourDesMots = {
+        siteImages: ['../img/captures-sites/addm-accueil.png', '../img/captures-sites/addm-accueil-2.png', '../img/captures-sites/addm-search.png', '../img/captures-sites/addm-bookPage.png','../img/captures-sites/addm-search-dark.png', '../img/captures-sites/addm-accueil1-dark.png', '../img/captures-sites/addm-bookPage-dark.png'],
+        siteImagesMobile: ['../img/captures-sites/addm-accueil-1-mobile.png', '../img/captures-sites/addm-search-mobile.png', '../img/captures-sites/addm-search-2-mobile.png', '../img/captures-sites/addm-menu-mobile.png', '../img/captures-sites/addm-bookPage-mobile.png', '../img/captures-sites/addm-bookPage-dark-mobile.png', '../img/captures-sites/addm-search-dark-mobile.png', '../img/captures-sites/addm-search-2-dark-mobile.png'],
+    };
+
+    const ImageWindowOpandCo = document.querySelector('#image-windowProject1');
+    const ImageWindowAuDetourDesMots = document.querySelector('#image-windowAuDetourDesMots');
     
-    // Logic to change image every 0.5 seconds with slide effect
-     let imageIndex = 0;
-     setInterval(() => {
-         // Appliquer l'animation pour cacher l'image actuelle
-         const ImageWindowOpandCo = document.querySelector('#image-windowProject1');
-         ImageWindowOpandCo.style.animation = 'slideLeftToRight 1s forwards';  // Animation pour déplacer à gauche
+    // Fonction générique pour changer l'image
+    function updateImage(projetImageElem, objImages) {
+        let imageIndex = 0;
+        
+        setInterval(() => {
+            // Appliquer l'animation pour cacher l'image actuelle
+            projetImageElem.style.animation = 'slideLeftToRight 1s forwards';
 
-         setTimeout(() => {
-             // Changer l'image source
-             if (window.innerWidth < 768) {
-                 imageIndex = (imageIndex + 1) % cardOpAndCo.siteImagesMobile.length;  // Change index, loop back to 0 when reaching end
-                 ImageWindowOpandCo.setAttribute('src', cardOpAndCo.siteImagesMobile[imageIndex]);
-             } else {
-             imageIndex = (imageIndex + 1) % cardOpAndCo.siteImages.length;  // Change index, loop back to 0 when reaching end
-             ImageWindowOpandCo.setAttribute('src', cardOpAndCo.siteImages[imageIndex]); 
-             ImageWindowOpandCo.style.animation = '';  // Réinitialiser l'animation
-             }
-             // Appliquer l'animation pour faire arriver la nouvelle image
-             ImageWindowOpandCo.style.animation = 'slideRightToLeft 1s forwards'; 
-         }, 1000);  // Attendre la fin de l'animation précédente (500ms)
-     }, 7000);  // Changer l'image toutes les 1000ms (1 seconde)
+            setTimeout(() => {
+                // Choisir l'image à afficher en fonction de la taille de l'écran
+                const currentImages = window.innerWidth < 768 ? objImages.siteImagesMobile : objImages.siteImages;
+                imageIndex = (imageIndex + 1) % currentImages.length;  // Boucle sur les images
 
-     setInterval();
+                // Mettre à jour la source de l'image
+                projetImageElem.setAttribute('src', currentImages[imageIndex]);
+
+                // Réinitialiser l'animation et appliquer l'animation d'arrivée
+                projetImageElem.style.animation = '';  // Réinitialiser l'animation
+                projetImageElem.style.animation = 'slideRightToLeft 1s forwards';
+            }, 1000);  // Attendre la fin de l'animation précédente avant de changer l'image
+        }, 7000);  // Changer l'image toutes les 7 secondes
+    }
+
+    // Utiliser la fonction générique pour chaque projet
+    updateImage(ImageWindowOpandCo, cardOpAndCo);
+    updateImage(ImageWindowAuDetourDesMots, cardAuDetourDesMots);
 });
